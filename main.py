1655c793ac6dc5d7f9c8b799d488d486bb75cb6e
SickCrisp <crissie.nicholas.r@gmail.com>
2025-02-10 13:35:59 -0800

    10FEB2025: Adding L9 to Git

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ def main():
         file_contents = f.read()
         print(file_contents)
 
+def count_words():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        count = len(words)
+        print(count)
+
+
 if __name__ == "__main__":
     main()
+    count_words()
