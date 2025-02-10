45bbdcbb80a353caebea6e54b9954da99da730d0
SickCrisp <crissie.nicholas.r@gmail.com>
2025-02-10 14:24:24 -0800

    10FEB2025: Adding L10 to Git

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-
-def count_words():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        count = len(words)
-        print(count)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    char_count = get_num_chars(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document\n")
+    print(char_count)
 
 
-if __name__ == "__main__":
-    main()
-    count_words()
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+
+def get_num_chars(text):
+    lower_text = text.lower()
+    char_count = {}
+    for letter in lower_text:
+        if letter not in char_count:
+            char_count[letter] = 1
+        else:
+            char_count[letter] += 1
+    return char_count
+
+
+main()
